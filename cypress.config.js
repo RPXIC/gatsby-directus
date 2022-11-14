d011b7e913be18968844bce23201dc01e0c15bee
rpxic <rpc2works@gmail.com>
2022-11-14 22:29:17 +0100

    update ci test config

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,11 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8000',
     specPattern: 'cypress/e2e',
-    projectId: 'yn556e'
+    projectId: 'yn556e',
+    excludeSpecPattern: 'cypress/e2e/accesibility.test.js'
   },
   env: {
-    EMAIL: '',
-    PASSWORD: ''
+    EMAIL: 'test@gmail.com',
+    PASSWORD: '123456'
   }
 })
